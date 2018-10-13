fc7d86282c9522521793b7a7f483e1e07d1470f7
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2018-10-13 23:41:17 +0200

    Added expect platform :)

--- common/src/main/kotlin/com/votlin/presentation/Shared.kt
+++ common/src/main/kotlin/com/votlin/presentation/Shared.kt
@@ -1,3 +1,7 @@
 package com.votlin.presentation
 
-data class Shared(val text: String = "HELLO multiplatform! :)")
+data class Shared(val text: String = "HELLO ${Platform.name}! :)")
+
+expect object Platform {
+    val name: String
+}
---
+++ votlin-js/src/main/kotlin/com/votlin/presentation/Platform.kt
@@ -0,0 +1,5 @@
+package com.votlin.presentation
+
+actual object Platform {
+    actual val name: String = "Web"
+}
