aac49b5500013efe2f36abc0398bc20e7ef8be78
Sergio Casero hernández <sergio.casero23@gmail.com>
2018-10-14 13:35:17 +0200

    Added domain models

--- android/app/src/main/java/com/votlin/android/ui/MainActivity.kt
+++ android/app/src/main/java/com/votlin/android/ui/MainActivity.kt
@@ -3,22 +3,12 @@ package com.votlin.android.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.votlin.android.R
-import com.votlin.client.presentation.PresenterView
-import com.votlin.client.presentation.Shared
-import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity(), PresenterView {
-
-    private val presenter = Shared(this, "Android")
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        presenter.doShomething()
-    }
-
-    override fun showText(text: String) {
-        this.text.text = text
     }
 
 
