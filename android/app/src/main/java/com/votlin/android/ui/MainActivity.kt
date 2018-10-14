463ac90cbf63819f5d363f67557c8c9591348a83
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2018-10-14 19:12:56 +0200

    Added Splash screen, remove old icons

--- android/app/src/main/java/com/votlin/android/ui/MainActivity.kt
+++ android/app/src/main/java/com/votlin/android/ui/MainActivity.kt
@@ -1,15 +1,46 @@
 package com.votlin.android.ui
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.bind
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.provider
 import com.votlin.android.R
+import com.votlin.android.error.AndroidErrorHandler
+import com.votlin.client.presentation.TalksPresenter
+import com.votlin.client.presentation.TalksView
+import com.votlin.model.Talk
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RootActivity<TalksView>(),TalksView {
+    override val presenter: TalksPresenter by instance()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+   override val layoutResourceId: Int = R.layout.activity_talks
+    override val activityModule: Kodein.Module = Kodein.Module{
+        bind<TalksPresenter>() with provider {
+            TalksPresenter(errorHandler = AndroidErrorHandler(),
+                    view =this@MainActivity)
+        }
     }
 
+    override fun initializeUI() {
+        // Nothing to do yet
+    }
+
+    override fun registerListeners() {
+        // Nothing to do yet.
+    }
+
+    override fun showProgress() {
+        // Nothing to do yet
+    }
+
+    override fun hideProgress() {
+        // Nothing to do yet
+    }
+
+    override fun showTalks(talks: List<Talk>) {
+        // Nothing to do yet
+        Log.i("Main activity", talks.toString())
+    }
 
 }
---
+++ common/src/main/kotlin/com/votlin/client/presentation/SplashPresenter.kt
@@ -0,0 +1,28 @@
+package com.votlin.client.presentation
+
+import com.votlin.client.domain.error.ErrorHandler
+
+class SplashPresenter(view: SplashView, errorHandler: ErrorHandler):
+        Presenter<SplashView>(view = view, errorHandler = errorHandler){
+
+    companion object {
+        const val SPLASH_MILISECONDS: Long = 1000
+    }
+
+    override fun initialize() {
+        view.showLoadingProgress(SPLASH_MILISECONDS)
+    }
+
+    override fun destroy() {
+        // Nothing to do yet
+    }
+
+    fun onRunnableCallback() {
+        view.goToTalksScreen()
+    }
+}
+
+interface SplashView: Presenter.View{
+    fun showLoadingProgress(delayMillis: Long)
+    fun goToTalksScreen()
+}
