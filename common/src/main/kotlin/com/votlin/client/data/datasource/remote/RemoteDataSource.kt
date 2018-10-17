6c755c10e1fade064dc1de022a9b36e050954d01
Sergio Casero hernández <sergio.casero23@gmail.com>
2018-10-17 20:49:59 +0200

    Type refactor!

--- common/src/main/kotlin/com/votlin/client/data/datasource/remote/RemoteDataSource.kt
+++ common/src/main/kotlin/com/votlin/client/data/datasource/remote/RemoteDataSource.kt
@@ -7,6 +7,6 @@ import com.votlin.model.Track
 interface RemoteDataSource {
     suspend fun getTalks(): List<Talk>
     suspend fun getTalk(talkId: Int): Talk
-    suspend fun getTalksByTrack(track: Track): List<Talk>
-    suspend fun rateTalk(rate: Rate): Unit
+    suspend fun getTalksByTrack(track: String): List<Talk>
+    suspend fun rateTalk(rate: Rate)
 }
