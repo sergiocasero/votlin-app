b83bdab45e9d361a45d196b0938d6b79fb736372
Sergio Casero hernández <sergio.casero23@gmail.com>
2018-10-21 20:10:03 +0200

    Remote data source is shared between Android and JS!!

--- android/app/src/main/java/com/votlin/android/di.kt
+++ android/app/src/main/java/com/votlin/android/di.kt
@@ -5,11 +5,11 @@ import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.singleton
-import com.votlin.android.api.AndroidRemoteDataSource
 import com.votlin.android.error.AndroidErrorHandler
 import com.votlin.android.executor.AndroidExecutor
 import com.votlin.android.storage.AndroidLocalDataSource
 import com.votlin.client.data.datasource.local.LocalDataSource
+import com.votlin.client.data.datasource.remote.CommonRemoteDataSource
 import com.votlin.client.data.datasource.remote.RemoteDataSource
 import com.votlin.client.data.repository.CommonRepository
 import com.votlin.client.domain.error.ErrorHandler
@@ -19,7 +19,7 @@ import com.votlin.client.domain.repository.Repository
 fun app(context: Context) = Kodein.Module {
     bind<Context>() with singleton { context }
     bind<LocalDataSource>() with singleton { AndroidLocalDataSource(context = instance()) }
-    bind<RemoteDataSource>() with singleton { AndroidRemoteDataSource() }
+    bind<RemoteDataSource>() with singleton { CommonRemoteDataSource() }
     bind<Repository>() with singleton { CommonRepository(remote = instance(), local = instance()) }
     bind<Executor>() with singleton { AndroidExecutor() }
     bind<ErrorHandler>() with singleton { AndroidErrorHandler() }
--- common/src/main/kotlin/com/votlin/client/data/datasource/remote/CommonRemoteDataSource.kt
+++ common/src/main/kotlin/com/votlin/client/data/datasource/remote/CommonRemoteDataSource.kt
@@ -8,22 +8,26 @@ import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.http.takeFrom
+import kotlinx.serialization.json.JSON
 
-abstract class CommonRemoteDataSource : RemoteDataSource {
+class CommonRemoteDataSource : RemoteDataSource {
 
     private val endPoint: String = "http://sergiocasero.es:10000"
 
-    abstract val client: HttpClient
+    private val client: HttpClient = HttpClient()
 
-    override suspend fun getTalks(): List<Talk> = client.get<TalksResponse> { apiUrl("talk") }.talks
+    override suspend fun getTalks(): List<Talk> =
+            JSON.parse<TalksResponse>(client.get { apiUrl("talk") }).talks
 
-    override suspend fun getTalk(talkId: Int): Talk = client.get { apiUrl("talk/$talkId") }
+    override suspend fun getTalk(talkId: Int): Talk =
+            JSON.parse(client.get { apiUrl("talk/$talkId") })
 
-    override suspend fun getTalksByTrack(track: String): List<Talk> = client.get<TalksResponse> { apiUrl("talk/$track") }.talks
+    override suspend fun getTalksByTrack(track: String): List<Talk> =
+            JSON.parse<TalksResponse>(client.get { apiUrl("talk/$track") }).talks
 
     override suspend fun rateTalk(rate: Rate): Unit = client.post {
         apiUrl("talk")
-        body = rate
+        body = JSON.stringify(rate)
     }
 
     protected fun HttpRequestBuilder.apiUrl(path: String) {
