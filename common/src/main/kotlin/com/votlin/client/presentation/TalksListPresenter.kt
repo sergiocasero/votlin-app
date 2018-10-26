5059163ef4656617fdf87221cb60c39f1e4dcda8
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2018-10-26 19:33:02 +0200

    Added iOS mock data for EDD talk (Network still not working trying to compile for iOS)

--- common/src/main/kotlin/com/votlin/client/presentation/TalksListPresenter.kt
+++ common/src/main/kotlin/com/votlin/client/presentation/TalksListPresenter.kt
@@ -34,51 +34,92 @@ class TalksListPresenter(view: TalksListView, errorHandler: ErrorHandler) :
     // TODO: remove this
     private fun mockTalks() = listOf(
             Talk(id = 0,
-                    name = "Talk 1",
-                    description = "Description 1",
+                    name = "Registro",
+                    description = "",
+                    speakers = listOf(),
+                    track = Track.ALL,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Bienvenida",
+                    description = "",
+                    speakers = listOf(),
+                    track = Track.ALL,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Tecnología y negocio: de una idea innovadora a un producto de éxito",
+                    description = "",
                     speakers = listOf(
-                            Speaker("", "", "Name 1", "", ""),
-                            Speaker("", "", "Name 2", "", "")
-                    ),
-                    track = Track.MAKER,
-                    time = Time(123123, 123123)),
-            Talk(id = 1,
-                    name = "Talk 2",
-                    description = "Description 2",
-                    speakers = listOf(
-                            Speaker("", "", "Name 1", "", ""),
-                            Speaker("", "", "Name 2", "", "")
-                    ),
-                    track = Track.DEVELOPMENT,
-                    time = Time(123123, 123123)),
-            Talk(id = 2,
-                    name = "Talk 3",
-                    description = "Description 3",
-                    speakers = listOf(
-                            Speaker("", "", "Name 1", "", ""),
-                            Speaker("", "", "Name 2", "", "")
+                            Speaker("", "", "Joaquín Álviz Martín", "", "")
                     ),
                     track = Track.BUSINESS,
-                    time = Time(123123, 123123)),
-            Talk(id = 3,
-                    name = "Talk 4",
-                    description = "Description 4",
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Robótica: experiencia y retos futuros",
+                    description = "",
                     speakers = listOf(
-                            Speaker("", "", "Name 1", "", ""),
-                            Speaker("", "", "Name 2", "", "")
-                    ),
-                    track = Track.MAKER,
-                    time = Time(123123, 123123)),
-            Talk(id = 4,
-                    name = "Talk 5",
-                    description = "Description 5",
-                    speakers = listOf(
-                            Speaker("", "", "Name 1", "", ""),
-                            Speaker("", "", "Name 2", "", "")
+                            Speaker("", "", "Concepción Alicia Monje", "", "")
                     ),
                     track = Track.DEVELOPMENT,
-                    time = Time(123123, 123123))
-    )
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "¿Qué es un fablab?",
+                    description = "",
+                    speakers = listOf(
+                            Speaker("", "", "Juan Carlos Cano", "", "")
+                    ),
+                    track = Track.MAKER,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Break",
+                    description = "",
+                    speakers = listOf(),
+                    track = Track.ALL,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Adventuras y desventuras de una spin-off universitaria",
+                    description = "",
+                    speakers = listOf(
+                            Speaker("", "", "Fernández Sánchez Figueroa", "", "")
+                    ),
+                    track = Track.BUSINESS,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Hablando de Big Data",
+                    description = "",
+                    speakers = listOf(
+                            Speaker("", "", "Sandra Navarro Nieto", "", "")
+                    ),
+                    track = Track.BUSINESS,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Devops: orígnees, realidades y mitos",
+                    description = "",
+                    speakers = listOf(
+                            Speaker("", "", "Francisco Rodríguez Domínguez", "", "")
+                    ),
+                    track = Track.DEVELOPMENT,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Aplicaciones multiplataforma con apacha cordova",
+                    description = "",
+                    speakers = listOf(
+                            Speaker("", "", "Domingo M. Solomando Chamizo", "", "")
+                    ),
+                    track = Track.DEVELOPMENT,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Charla: Presentación de asociaciones",
+                    description = "",
+                    speakers = listOf(),
+                    track = Track.MAKER,
+                    time = Time(1540573874942, 1540573874942)),
+            Talk(id = 0,
+                    name = "Break",
+                    description = "",
+                    speakers = listOf(),
+                    track = Track.ALL,
+                    time = Time(1540573874942, 1540573874942))
+            )
 
 }
 
