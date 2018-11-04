9ebfdca0cdc4ec587d588796d0815b842d70ac23
Sergio Casero hernández <sergio.casero23@gmail.com>
2018-11-04 18:58:57 +0100

    Added arrow dependencies

--- common/src/main/kotlin/com/votlin/client/data/datasource/remote/RemoteDataSource.kt
+++ common/src/main/kotlin/com/votlin/client/data/datasource/remote/RemoteDataSource.kt
@@ -2,7 +2,6 @@ package com.votlin.client.data.datasource.remote
 
 import com.votlin.model.Rate
 import com.votlin.model.Talk
-import com.votlin.model.Track
 
 interface RemoteDataSource {
     suspend fun getTalks(): List<Talk>
